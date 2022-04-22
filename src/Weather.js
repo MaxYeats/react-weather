37554f5947e477463dc216f0930248521e07983d
Max Yeats <90226233+MaxYeats@users.noreply.github.com>
2022-04-21 20:01:18 -0400

    Formatting the rows and columns
    Rows and columns were formatted for the data to fit in. Alignments and distances between the columns were issues. Added Google fonts (Bootstrap was added in previous commit - my mistake not including this before)

--- src/Weather.js
+++ src/Weather.js
@@ -3,7 +3,7 @@
 import React from "react";
 import axios from "axios";
 import "./App.css";
-//import { SpinnerRoundOutlined } from "spinners-react";
+import { SpinnerRoundOutlined } from "spinners-react";
 import { useState } from "react";
 import ReactAnimatedWeather from "react-animated-weather";
 
@@ -29,6 +29,8 @@ export default function Weather() {
     setLoaded(true);
     setWeather({
       temperature: response.data.main.temp,
+      tempmax: response.data.main.temp_max,
+      tempmin: response.data.main.temp_min,
       description: response.data.weather[0].description,
       humidity: response.data.main.humidity,
       wind: response.data.wind.speed,
@@ -36,7 +38,7 @@ export default function Weather() {
         <ReactAnimatedWeather
           icon="CLEAR_DAY"
           color="white"
-          size={64}
+          size={70}
           animate="true"
         />
       ),
@@ -45,15 +47,16 @@ export default function Weather() {
   }
 
   let form = (
-    <form onSubmit={startSearch}>
+    <form className="react-form" onSubmit={startSearch}>
       <input
+        className="react-input"
         type="search"
         placeholder="Enter your city here"
         autoComplete={false}
         autoFocus={true}
         onChange={findCity}
       ></input>
-      <input type="submit" value="Search"></input>
+      <input className="search-button" type="submit" value="Search"></input>
     </form>
   );
 
@@ -61,85 +64,41 @@ export default function Weather() {
     return (
       <div>
         {form}
-        <div className="container-1">
-          <div className="row">
-            <h2>{city}</h2>
-            <h2>{Math.round(weather.temperature)}°C</h2>
-            <div>{weather.description}</div>
+        <div className="container">
+          <div className="city-name">{city}</div>
+          <div className="date">
+            Monday, 5/1/2022 <span className="time">10:00</span>
           </div>
           <div className="row">
-            <div className="col-4">
-              <div>{weather.icon}</div>
+            <div className="col-3"></div>
+            <div className="col-3">
+              <div className="main-temperature">
+                {Math.round(weather.temperature)}°C
+              </div>
+              <div className="minmax-temp">
+                Min.{Math.round(weather.tempmin)}°C | Max.
+                {Math.round(weather.tempmax)}°C{" "}
+              </div>
             </div>
 
-            <div className="col-8">
-              <div>Humidity: {weather.humidity}%</div>
-              <div>Wind: {Math.round(weather.wind)}m/s</div>
+            <div className="col-3">
+              <div className="icon">{weather.icon}</div>
+              <div className="description">{weather.description}</div>
+              <div className="humidity">Humidity: {weather.humidity}%</div>
+              <div className="wind">Wind: {Math.round(weather.wind)}m/s</div>
             </div>
-          </div>
-        </div>
-        <div className="container-2">
-          <div classname="row">
-            <div classname="col-2">
-              {" "}
-              <ReactAnimatedWeather
-                icon="CLEAR_DAY"
-                color="white"
-                size={40}
-                animate="true"
-              />
-            </div>
-            <div classname="col-2">
-              {" "}
-              <ReactAnimatedWeather
-                icon="CLEAR_DAY"
-                color="white"
-                size={40}
-                animate="true"
-              />
-            </div>
-            <div classname="col-2">
-              {" "}
-              <ReactAnimatedWeather
-                icon="CLEAR_DAY"
-                color="white"
-                size={40}
-                animate="true"
-              />
-            </div>
-            <div classname="col-2">
-              {" "}
-              <ReactAnimatedWeather
-                icon="CLEAR_DAY"
-                color="white"
-                size={40}
-                animate="true"
-              />
-            </div>
-            <div classname="col-2">
-              {" "}
-              <ReactAnimatedWeather
-                icon="CLEAR_DAY"
-                color="white"
-                size={40}
-                animate="true"
-              />
-            </div>
-            <div classname="col-2">
-              {" "}
-              <ReactAnimatedWeather
-                icon="CLEAR_DAY"
-                color="white"
-                size={40}
-                animate="true"
-              />
-            </div>
+            <div className="col-3"></div>
           </div>
         </div>
       </div>
     );
   } else {
-    return form;
+    return (
+      <div>
+        {form}
+        <SpinnerRoundOutlined size="30%" thickness="30" color="5EE6EB" />
+      </div>
+    );
   }
 
   //return <SpinnerRoundOutlined size="30%" thickness="30" color="5EE6EB" />;
